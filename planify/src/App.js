77e88b250d296e5abb2af5ba7ddb8849cd8def69
HE161655-BuiTuanSon <sonbthe161655@fpt.edu.vn>
2025-02-26 11:30:19 +0700

    add folder & headerMC

--- planify/src/App.js
+++ planify/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/Author/Login";
 import Authorization from "./components/Authorization";
+import HeaderManagerCampus from "./components/Header/HeaderManagerCampus";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/authorization" element={<Authorization />} />
+        <Route path="/homeMC" element={<HeaderManagerCampus />}></Route>
       </Routes>
     </div>
   );
