0449075d51b698840f37e3c9b0d1e8e8dde55145
HE161655-BuiTuanSon <sonbthe161655@fpt.edu.vn>
2025-02-27 14:41:24 +0700

    fix header & footer

---
+++ planify/src/pages/Author/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+function Home(props) {
+  return (
+    <div>
+      <Header></Header>
+      <h1>hehehe</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default Home;
