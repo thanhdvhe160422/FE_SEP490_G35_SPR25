e3712e377e5517cdc9f168ab4484626ba16efb22
Thu <thunvhe176252@fpt.edu.vn>
2025-03-05 13:48:58 +0700

    test call api

--- planify/src/pages/Events/EventDetailSpec.jsx
+++ planify/src/pages/Events/EventDetailSpec.jsx
@@ -1,41 +1,24 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "../../styles/Events/EventDetailSpec.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import {getEventDetailSpec} from "../../services/EventService";
 
 function EventDetailSpec() {
-  const event = {
-    id: "1",
-    Status: "Running",
-    EventTitle: "Sự kiện Tết",
-    StartTime: "01-01-2020",
-    EndTime: "03-01-2020",
-    EventDescription: "Sự kiện diễn ra hàng năm với nhiều hoạt động thú vị.",
-    Placed: "Phố đi bộ Nguyễn Huệ",
-    CategoryViewModel: { Id: "1", Name: "Lễ hội" },
-    EventMedias: [
-      {
-        Id: "1",
-        MediaDTO: {
-          Url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFYDy0dtqY-GmpwssQR-DKlQgrvsRy47FokQ&s",
-        },
-      },
-      {
-        Id: "2",
-        MediaDTO: {
-          Url: "https://blog.topcv.vn/wp-content/uploads/2021/07/sk2uEvents_Page_Header_2903ed9c-40c1-4f6c-9a69-70bb8415295b.jpg",
-        },
-      },
-      {
-        Id: "3",
-        MediaDTO: {
-          Url: "https://cdn.prod.website-files.com/6769617aecf082b10bb149ff/67763d8a2775bee07438e7a5_Events.png",
-        },
-      },
-    ],
-  };
-
-  const images = event.EventMedias.map((media) => media.MediaDTO.Url);
+    const [event, setEvent] = useState(null);
+    useEffect(() => {
+      const fetchEventDetail = async(eventId)=>{
+        try{
+            const data = await getEventDetailSpec(eventId);
+            setEvent(data);
+            console.log(event)
+        }catch(error){
+            console.error("Error loading event detail spectator: ",error);
+        }
+      }
+      fetchEventDetail(2);
+    });
+  const images = event?.EventMedias?.map((media) => media.MediaDTO.Url);
   const [activeIndex, setActiveIndex] = useState(0);
 
   const changeSlide = (index) => {
--- planify/src/services/EventService.jsx
+++ planify/src/services/EventService.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:4000/events";
+const API_EVENT_DETAIL_SPEC = "https://localhost:44320/api/EventForSpectators";
 
 const getPosts = async () => {
   try {
@@ -11,5 +12,14 @@ const getPosts = async () => {
     return [];
   }
 };
+export const getEventDetailSpec = async(eventId) =>{
+  try{
+    const response = await axios.get(API_EVENT_DETAIL_SPEC+'/'+eventId);
+    return response.data;
+  }catch(error){
+    console.error("Error fetching event detail spectator: ", error);
+    return [];
+  }
+}
 
 export default getPosts;
