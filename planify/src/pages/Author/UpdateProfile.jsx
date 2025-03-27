0a4d3614a43f7a92b404b2ff856c33bc364b0baa
Thu <thunvhe176252@fpt.edu.vn>
2025-03-27 13:14:02 +0700

    fix update gender

--- planify/src/pages/Author/UpdateProfile.jsx
+++ planify/src/pages/Author/UpdateProfile.jsx
@@ -5,7 +5,7 @@ import Header from "../../components/Header/Header";
 import { useSnackbar } from "notistack";
 import { updateProfile,updateAvatar, getProfileById } from "../../services/userService";
 import{getProvinces, getDistricts, getWards} from "../../services/addressService";
-import "../../styles/Author/LoadingImage.css"
+import "../../styles/Author/Loading.css"
 
 const UpdateProfile = () => {
   const [user, setUser] = useState(null);
@@ -51,8 +51,7 @@ const UpdateProfile = () => {
     const fetchProvinces = async () => {
       try {
         const data = await getProvinces();
-        setProvinces(data.data.result || []);
-        await setSelectedProvince(1);
+        await setProvinces(data.data.result || []);
       } catch (error) {
         console.error("Lỗi lấy danh sách tỉnh:", error);
         setProvinces([]);
@@ -62,6 +61,9 @@ const UpdateProfile = () => {
     fetchProvinces();
     fetchUserData();
   }, []);
+  useEffect(() => {
+    console.log("Updated user state:", user);
+  }, [user]);
 
   useEffect(() => {
     if (!selectedProvince) return;
@@ -69,8 +71,7 @@ const UpdateProfile = () => {
     const fetchDistricts = async () => {
       try {
         const data = await getDistricts(selectedProvince);
-        setDistricts(data.data.result || []);
-        await setSelectedDistrict(1);
+        await setDistricts(data.data.result || []);
         setWards([]);
       } catch (error) {
         console.error("Lỗi lấy danh sách quận/huyện:", error);
@@ -87,8 +88,7 @@ const UpdateProfile = () => {
     const fetchWards = async () => {
       try {
         const data = await getWards(selectedDistrict)
-        setWards(data.data .result|| []);
-        await setSelectedWard(1);
+        await setWards(data.data .result|| []);
       } catch (error) {
         console.error("Lỗi lấy danh sách phường/xã:", error);
         setWards([]);
@@ -126,7 +126,7 @@ const UpdateProfile = () => {
         phoneNumber: user.phoneNumber,
         addressId: user.addressId,
         avatarId: user.avatarId,
-        gender: user.gender===1,
+        gender: user.gender+""==='true',
         addressVM: {
           id: user.addressVM?.id || 0,
           addressDetail: user?.addressVM?.addressDetail || "",
@@ -144,14 +144,12 @@ const UpdateProfile = () => {
           }
         }
       };
-
       const parts = updatedUser.dateOfBirth.split("T")[0].split("-");
       const day = parts[2];
       const month = parts[1];
       const year = parts[0];
       const formattedDate = `${year}-${month}-${day}`;
       updatedUser.dateOfBirth = formattedDate;
-      console.log(updatedUser);
       const token = localStorage.getItem("token");
       if (!token) {
         enqueueSnackbar("Please login again", {
@@ -241,8 +239,6 @@ const UpdateProfile = () => {
     }
   };
 
-  if (loading) return <div className="loader"></div>
-  if (!user) return <p>No user data found</p>;
 
   function convertToDirectLink(googleDriveUrl) {
     
@@ -250,6 +246,8 @@ const UpdateProfile = () => {
     const fileId = googleDriveUrl.split("id=")[1];
     return `https://drive.google.com/thumbnail?id=${fileId}&sz=w1000`;
 }
+  if (loading) return <div className="loader"></div>
+  if (!user) return <p>No user data found</p>;
 
   return (
     <>
@@ -333,10 +331,10 @@ const UpdateProfile = () => {
                     <input
                       type="radio"
                       name="gender"
-                      value="1"
-                      checked={Number(user.gender) === 1}
+                      value="true"
+                      checked={user.gender+"" === "true"}
                       onChange={(e) =>
-                        setUser({ ...user, gender: Number(e.target.value) })
+                        setUser({ ...user, gender: e.target.value })
                       }
                     />
                     Male
@@ -345,10 +343,10 @@ const UpdateProfile = () => {
                     <input
                       type="radio"
                       name="gender"
-                      value="0"
-                      checked={Number(user.gender) === 0}
+                      value="false"
+                      checked={user.gender+"" === "false"}
                       onChange={(e) =>
-                        setUser({ ...user, gender: Number(e.target.value) })
+                        setUser({ ...user, gender: e.target.value })
                       }
                     />
                     Female
