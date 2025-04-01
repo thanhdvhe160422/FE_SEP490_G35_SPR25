59f3dda931f249b9118b90e18dc730a9238b0d0f
Dong Van Thanh <thanhdvhe160422@fpt.edu.vn>
2025-04-01 23:48:17 +0700

    update code create plan

--- planify/src/pages/Events/UpdateEvent.jsx
+++ planify/src/pages/Events/UpdateEvent.jsx
@@ -4,13 +4,13 @@ import { useSnackbar } from "notistack";
 import "../../styles/Events/UpdateEvent.css";
 import Swal from "sweetalert2";
 import Header from "../../components/Header/Header";
-import { getEventEOGById,updateEvent } from "../../services/EventService";
+import { getEventEOGById, updateEvent } from "../../services/EventService";
 import { getCategoryByCampusId } from "../../services/CategoryService";
 import { getCampusIdByName } from "../../services/campusService";
 const UpdateEventForm = () => {
   const { enqueueSnackbar } = useSnackbar();
-  const {id} = useParams();
-  const [categories,setCategories] = useState([]);
+  const { id } = useParams();
+  const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(1);
   const [event, setEvent] = useState({
     EventTitle: "Annual Tech Meetup",
@@ -37,10 +37,9 @@ const UpdateEventForm = () => {
     TargetAudience: "University students and alumni",
     SloganEvent: "Innovate. Connect. Grow.",
   });
-  useEffect(()=>{
-    
-    const fetchCategoryData = async()=>{
-      try{
+  useEffect(() => {
+    const fetchCategoryData = async () => {
+      try {
         const campustName = localStorage.getItem("campus");
         const campusId = await getCampusIdByName(campustName);
         const response = await getCategoryByCampusId(campusId.id);
@@ -48,8 +47,8 @@ const UpdateEventForm = () => {
       } catch (error) {
         console.error("Error fetching category data:", error);
       }
-    }
-    const fetchEventData = async ()=>{
+    };
+    const fetchEventData = async () => {
       try {
         const response = await getEventEOGById(id);
         setEvent(response.result);
@@ -57,10 +56,10 @@ const UpdateEventForm = () => {
       } catch (error) {
         console.error("Error fetching event data:", error);
       }
-    }
+    };
     fetchCategoryData();
     fetchEventData();
-  },[id])
+  }, [id]);
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setEvent((prev) => ({
@@ -71,7 +70,7 @@ const UpdateEventForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(selectedCategory)
+    console.log(selectedCategory);
     const eventData = {
       id: event.id,
       eventTitle: event.eventTitle,
@@ -79,29 +78,29 @@ const UpdateEventForm = () => {
       startTime: event.startTime,
       endTime: event.endTime,
       amountBudget: event.amountBudget,
-      isPublic: event.isPublic ? 1 : 0, 
+      isPublic: event.isPublic ? 1 : 0,
       timePublic: event.timePublic,
       status: event.status,
-      managerId: event?.manager.id||"",
+      managerId: event?.manager.id || "",
       campusId: 1,
       categoryEventId: selectedCategory,
       placed: event.placed,
       createdAt: event.createdAt,
-      createBy: event?.createdBy.id||"",
-      measuringSuccess: event?.measuringSuccess||"",
-      goals: event?.goals||"",
-      monitoringProcess: event?.monitoringProcess||"",
-      sizeParticipants: event?.sizeParticipants||0,
-      promotionalPlan: event?.promotionalPlan||"",
-      targetAudience: event?.targetAudience||"",
-      sloganEvent: event?.sloganEvent||"",
+      createBy: event?.createdBy.id || "",
+      measuringSuccess: event?.measuringSuccess || "",
+      goals: event?.goals || "",
+      monitoringProcess: event?.monitoringProcess || "",
+      sizeParticipants: event?.sizeParticipants || 0,
+      promotionalPlan: event?.promotionalPlan || "",
+      targetAudience: event?.targetAudience || "",
+      sloganEvent: event?.sloganEvent || "",
     };
 
     console.log("Updated Event:", eventData);
 
     try {
-      const response = await updateEvent(event.id,eventData);
-      if (response.status===200) {
+      const response = await updateEvent(event.id, eventData);
+      if (response.status === 200) {
         Swal.fire({
           title: "Event Updated Successfully",
           icon: "success",
@@ -128,8 +127,7 @@ const UpdateEventForm = () => {
         icon: "error",
       });
     }
-};
-
+  };
 
   return (
     <>
@@ -153,16 +151,16 @@ const UpdateEventForm = () => {
           </div>
 
           <div className="form-floating-group">
-            <textarea
-              name="EventDescription"
-              id="EventDescription"
+            <input
+              type="text"
+              name="Placed"
+              id="Placed"
               className="floating-input"
-              value={event?.eventDescription}
+              value={event?.placed}
               onChange={handleChange}
-              placeholder="Event Description"
-              rows={3}
+              placeholder="Location"
             />
-            <label className="floating-label">Event Description</label>
+            <label className="floating-label">Location</label>
           </div>
 
           <div className="form-floating-group">
@@ -204,105 +202,34 @@ const UpdateEventForm = () => {
             <label className="floating-label">Budget Amount</label>
           </div>
 
-          {/* <div className="form-floating-group">
-          <input
-            type="datetime-local"
-            name="TimePublic"
-            id="TimePublic"
-            className="floating-input"
-            value={event.TimePublic}
-            onChange={handleChange}
-            placeholder="Public Time"
-          />
-          <label className="floating-label">Public Time</label>
-        </div> */}
-
-          {/* <div className="form-floating-group">
-          <input
-            type="text"
-            name="Status"
-            id="Status"
-            className="floating-input"
-            value={event.Status}
-            onChange={handleChange}
-            placeholder="Status"
-          />
-          <label className="floating-label">Status</label>
-        </div> */}
-
-          {/* <div className="form-floating-group">
-          <input
-            type="number"
-            name="ManagerId"
-            id="ManagerId"
-            className="floating-input"
-            value={event.ManagerId}
-            onChange={handleChange}
-            placeholder="Manager ID"
-          />
-          <label className="floating-label">Manager ID</label>
-        </div> */}
-
-          {/* <div className="form-floating-group">
-            <input
-              name="Campus"
-              id="Campus"
-              className="floating-input"
-              value={event?.Campus}
-              onChange={handleChange}
-              placeholder="Campus"
-            />
-            <label className="floating-label">Campus</label>
-          </div> */}
-
           <div className="form-floating-group">
-            {/* <input
-              type="number"
+            <select
               name="CategoryEventId"
               id="CategoryEventId"
               className="floating-input"
-              value={event?.CategoryEventId}
-              onChange={handleChange}
-              placeholder="Category Event ID"
-            /> */}
-            
-            <select
-                  className="input-category"
-                  value={selectedCategory}
-                  onChange={(e) => setSelectedCategory(e.target.value)}
-                >
-                  {categories.map((c) => (
-                    <option key={c.id} value={c.id}>
-                      {c.categoryEventName}
-                    </option>
-                  ))}
-                </select>
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              {categories.map((c) => (
+                <option key={c.id} value={c.id}>
+                  {c.categoryEventName}
+                </option>
+              ))}
+            </select>
             <label className="floating-label">Category Event</label>
           </div>
 
           <div className="form-floating-group">
-            <input
-              type="text"
-              name="Placed"
-              id="Placed"
+            <textarea
+              name="EventDescription"
+              id="EventDescription"
               className="floating-input"
-              value={event?.placed}
+              value={event?.eventDescription}
               onChange={handleChange}
-              placeholder="Location"
+              placeholder="Event Description"
+              rows={3}
             />
-            <label className="floating-label">Location</label>
-          </div>
-
-          <div className="form-group">
-            <label className="field-label">Is Public</label>
-            <input
-              type="checkbox"
-              name="IsPublic"
-              id="IsPublic"
-              className="form-check-input"
-              checked={event?.isPublic}
-              onChange={handleChange}
-            />
+            <label className="floating-label">Event Description</label>
           </div>
 
           <div className="form-floating-group">
@@ -319,32 +246,6 @@ const UpdateEventForm = () => {
           </div>
 
           <div className="form-floating-group">
-            <textarea
-              name="Goals"
-              id="Goals"
-              className="floating-input"
-              value={event?.goals}
-              onChange={handleChange}
-              placeholder="Event Goals"
-              rows={2}
-            />
-            <label className="floating-label">Goals</label>
-          </div>
-
-          <div className="form-floating-group">
-            <textarea
-              name="MonitoringProcess"
-              id="MonitoringProcess"
-              className="floating-input"
-              value={event?.monitoringProcess}
-              onChange={handleChange}
-              placeholder="Monitoring Process"
-              rows={2}
-            />
-            <label className="floating-label">Monitoring Process</label>
-          </div>
-
-          <div className="form-floating-group">
             <input
               type="number"
               name="SizeParticipants"
@@ -355,6 +256,31 @@ const UpdateEventForm = () => {
               placeholder="Number of Participants"
             />
             <label className="floating-label">Number of Participants</label>
+          </div>
+
+          <div className="form-floating-group">
+            <input
+              type="text"
+              name="SloganEvent"
+              id="SloganEvent"
+              className="floating-input"
+              value={event?.sloganEvent}
+              onChange={handleChange}
+              placeholder="Event Slogan"
+            />
+            <label className="floating-label">Slogan</label>
+          </div>
+          <div className="form-floating-group">
+            <textarea
+              name="Goals"
+              id="Goals"
+              className="floating-input"
+              value={event?.goals}
+              onChange={handleChange}
+              placeholder="Event Goals"
+              rows={2}
+            />
+            <label className="floating-label">Goals</label>
           </div>
 
           <div className="form-floating-group">
@@ -384,16 +310,16 @@ const UpdateEventForm = () => {
           </div>
 
           <div className="form-floating-group">
-            <input
-              type="text"
-              name="SloganEvent"
-              id="SloganEvent"
+            <textarea
+              name="MonitoringProcess"
+              id="MonitoringProcess"
               className="floating-input"
-              value={event?.sloganEvent}
+              value={event?.monitoringProcess}
               onChange={handleChange}
-              placeholder="Event Slogan"
+              placeholder="Monitoring Process"
+              rows={2}
             />
-            <label className="floating-label">Slogan</label>
+            <label className="floating-label">Monitoring Process</label>
           </div>
 
           <div className="text-end">
