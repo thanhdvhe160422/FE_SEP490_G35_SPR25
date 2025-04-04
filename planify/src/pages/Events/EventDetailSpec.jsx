6c2871be5afd810062d5baa804da2d6903dc8bf9
Thu <thunvhe176252@fpt.edu.vn>
2025-04-05 01:36:50 +0700

    cancel register participant

--- planify/src/pages/Events/EventDetailSpec.jsx
+++ planify/src/pages/Events/EventDetailSpec.jsx
@@ -14,7 +14,7 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import { RegisterParticipant } from "../../services/EventService";
+import { RegisterParticipant, DeleteRegisterParticipant, IsRegisterParticipant } from "../../services/EventService";
 import Swal from "sweetalert2";
 
 function EventDetailSpec() {
@@ -23,6 +23,7 @@ function EventDetailSpec() {
   const [event, setEvent] = useState(null);
   const [bannerImages, setBannerImages] = useState([]);
   const [openLightbox, setOpenLightbox] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
 
   // const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
@@ -51,8 +52,23 @@ function EventDetailSpec() {
         console.error("Failed to fetch event details:", error);
       }
     };
+    const fetchDataPaticipant = async () => {
+        try{
+          var userId = localStorage.getItem("userId");
+          const response = await IsRegisterParticipant(event.id,userId);
+          console.log("check resigter: "+JSON.stringify(response,null,2));
+          if (response.status===200)
+            setIsRegistered(true);
+          else{
+            setIsRegistered(false);
+          }
+        }catch{
+          setIsRegistered(false);
+        }
+    }
 
     fetchEventDetail();
+    fetchDataPaticipant();
   }, [eventId]);
 
   const statusEvent = (start, end) => {
@@ -86,14 +102,36 @@ function EventDetailSpec() {
       const response = await RegisterParticipant(eventId,userId);
       console.log(JSON.stringify(response,null,2))
       if (response.status===201){
-        console.log("register successfully");
+        setIsRegistered(true);
         Swal.fire("Success",
                   "Register successfully",
                   "success"
                   );
       }else{
         Swal.fire("Error",
-                  response?.status,
+                  response?.message,
+                  "error"
+                  );
+      }
+    }catch(error){
+      console.error(error);
+    }
+  }
+  async function handleCancelRegisterParticipant(){
+    try{
+      var eventId = event.id;
+      var userId = localStorage.getItem("userId")
+      const response = await DeleteRegisterParticipant(eventId,userId);
+      console.log(JSON.stringify(response,null,2))
+      if (response.status===200){
+        setIsRegistered(false);
+        Swal.fire("Success",
+                  "Cancel register successfully",
+                  "success"
+                  );
+      }else{
+        Swal.fire("Error",
+                  response?.message,
                   "error"
                   );
       }
@@ -155,10 +193,25 @@ function EventDetailSpec() {
           )}
         </div>
         <div style={{ marginRight: "70%" }}>
-          <button className="btn btn-warning" style={{ height: "50px" }}
-          onClick={HandleRegisterParticipant}>
+          {isRegistered ? (
+          <button
+            className="btn btn-danger"
+            style={{ height: "50px" }}
+            id="btn-cancel-register"
+            onClick={handleCancelRegisterParticipant}
+          >
+            Hủy tham gia
+          </button>
+        ) : (
+          <button
+            className="btn btn-warning"
+            style={{ height: "50px" }}
+            id="btn-register"
+            onClick={HandleRegisterParticipant}
+          >
             Đăng ký tham gia
           </button>
+        )}
         </div>
 
         <div className="event-details">
